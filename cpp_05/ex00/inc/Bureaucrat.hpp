3b95ffe6e5068db498b6313d4afa6d5699d56d12
Ben Golding <bgolding.42lausanne@gmail.com>
2024-11-04 11:50:01 +0100

    First basic implementations, not yet tested : lunch break.

--- cpp_05/ex00/inc/Bureaucrat.hpp
+++ cpp_05/ex00/inc/Bureaucrat.hpp
@@ -7,18 +7,43 @@
 # define HIGHEST_GRADE	(1)
 # define LOWEST_GRADE	(150)
 
+# define DEFAULT_NAME	"Unknown"
+# define DEFAULT_GRADE	(LOWEST_GRADE)
+
 class Bureaucrat
 {
 	public:
+		Bureaucrat();
 		Bureaucrat(std::string, int);
 		Bureaucrat(const Bureaucrat &other);
 		~Bureaucrat();
 
 		Bureaucrat &operator=(const Bureaucrat &rhs);
+
+		std::string getName() const;
+		int			getGrade() const;
 	
+		void		setGrade(int new_grade);
+		void		incrementGrade();
+		void		decrementGrade();
+
 	private:
 		const std::string	_name;
 		int					_grade;
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
 };
+
+std::ostream &operator<<(std::ostream &os ,const Bureaucrat &other);
 
 #endif
--- cpp_05/ex00/src/Bureaucrat.cpp
+++ cpp_05/ex00/src/Bureaucrat.cpp
@@ -1,28 +1,75 @@
 #include "Bureaucrat.hpp"
 
 //  ==========| CONSTRUCTORS |==========
+Bureaucrat::Bureaucrat() : _name(DEFAULT_NAME), _grade(DEFAULT_GRADE) {};
+
 Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
 {
-    
+	this->setGrade(grade);
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat &other)
 {
-    //  DEFAULT
+	*this = other;
 }
 
 //  ===========| DESTRUCTOR |===========
 Bureaucrat::~Bureaucrat()
 {
-    //  DEFAULT
+	return ;
 }
 
 //  =======| OPERATOR OVERLOADS |=======
 Bureaucrat &Bureaucrat::operator=(const Bureaucrat &rhs)
 {
-    //  DEFAULT
+	this->_grade = rhs._grade;
+	return (*this);
+}
+
+std::ostream &Bureaucrat::operator<<(std::operator &os, Bureaucrat const &other)
+{
+	// TODO
 }
 
 //  ============| METHODS |=============
 
+std::string Bureaucrat::getName() const
+{
+	return (_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (_grade);
+}
+
+void Bureaucrat::setGrade(int new_grade)
+{
+	if (new_grade < HIGHEST_GRADE)
+		throw GradeTooHighException();
+	if (new_grade > LOWEST_GRADE)
+		throw GradeTooLowException();
+	_grade = new_grade;
+}
+
+void Bureaucrat::incrementGrade()
+{
+	this->setGrade(this->getGrade() - 1);
+}
+
+void Bureaucrat::decrementGrade()
+{
+	this->setGrade(this->getGrade() + 1);
+}
+
 //  ========| VIRTUAL METHODS |=========
+
+const char  *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade too high");
+}
+
+const char  *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade too low");
+}
