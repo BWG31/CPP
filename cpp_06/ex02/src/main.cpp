c932fc497ea1f58276ed58160665c4d7cfc9e837
Ben Golding <bgolding.42lausanne@gmail.com>
2024-11-13 14:29:49 +0100

    cpp06: ex02: added generate() implementation

--- cpp_06/ex02/src/main.cpp
+++ cpp_06/ex02/src/main.cpp
@@ -3,8 +3,38 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+
+Base *generate(void);
 
 int main(void)
 {
+	Base *ptr = generate();
+	delete ptr;
 	return (0);
+}
+
+Base *generate(void)
+{
+	Base *ptr;
+
+	srand(time(NULL) + getpid());
+	int num = rand() % 3;
+	switch (num){
+		case 0:
+			ptr = new A;
+			break ;
+		case 1:
+			ptr = new B;
+			break ;
+		case 2:
+			ptr = new C;
+			break ;
+		default:
+			ptr = NULL;
+	}
+	std::cout << "num: " << num << std::endl;
+	return (ptr);
 }
