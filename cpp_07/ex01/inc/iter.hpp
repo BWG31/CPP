aba93696c689d14faa2629dc2458bbf888634728
Ben Golding <bgolding.42lausanne@gmail.com>
2024-11-22 15:04:25 +0100

    cpp07: ex01: added const compatibility
    iter() now also works with const T values.

--- cpp_07/ex01/inc/iter.hpp
+++ cpp_07/ex01/inc/iter.hpp
@@ -9,4 +9,10 @@ template <class T> void iter(T *array, size_t length, void (*f)(T&))
 		f(array[i]);
 }
 
+template <class T> void iter(const T *array, size_t length, void (*f)(const T&))
+{
+	for (size_t i = 0; i < length; i++)
+		f(array[i]);
+}
+
 #endif
